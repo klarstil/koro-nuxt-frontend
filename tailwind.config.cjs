41738ae67345dbc5ce148ae8e75b7d9ae87992fb
klarstil <klarstil@googlemail.com>
2023-06-28 11:37:40 +0200

    NTR - Hopefully fix class mismatch

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,14 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        borderWidth: {
+            DEFAULT: '1px',
+            0: '0',
+            2: '2px',
+            4: '4px',
+            6: '6px',
+            8: '8px',
+            12: '12px',
+        },
+    },
 };
